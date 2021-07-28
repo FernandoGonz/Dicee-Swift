403f2d9b09ba52302ca1feeaf6bf0c68a605ced8
Fernando Gonzalez <ing.fernandogonzalez@outlook.com>
2021-07-28 12:56:34 -0500

    UI elements referenced in viewController

--- dicee-Swift/ViewController.swift
+++ dicee-Swift/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference a UI (User Interface) element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Set a new image
+        // diceImageView1.image = Image Literal
+        
     }
 
 
